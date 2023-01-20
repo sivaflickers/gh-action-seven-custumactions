606ecb1baa3671781bab188b5a77d19880a66ebb
Siva <sivapill@in.ibm.com>
2023-01-21 00:26:51 +0530

    AWS S3 site hosting dynamic URL generation

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -10,10 +10,13 @@ const distFolder = core.getInput('dist-folder', {required:true});
 
 // 2) Upload Files
 const s3Uri = `s3://${bucket}`
-exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`)
+exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`);
 
 core.notice('Hello from my custom JavaScript Action!');
     
+const websiteUrl= `http://${bucket}.s3-website-${bucketRegion}.amazonaws.com`;
+core.setOutput('website-url', website-url); //::set-output
+
 }
 
 run();
